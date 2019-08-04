84c7d5b9e20ac3b932da68dc228f3b10973ec8f5
N-bred <thehardyboiz1@gmail.com>
2019-08-04 17:47:30 -0500

    Updates

---
+++ precache-manifest.cf92f93f835a75c601183d22c4d21525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c8c1a920da74f4aab79c7012e50f70",
+    "url": "/Movies-App/index.html"
+  },
+  {
+    "revision": "8346505a9826fdb6bf06",
+    "url": "/Movies-App/static/css/main.94be04ba.chunk.css"
+  },
+  {
+    "revision": "7655f998c0d1cdd16515",
+    "url": "/Movies-App/static/js/2.188a3839.chunk.js"
+  },
+  {
+    "revision": "8346505a9826fdb6bf06",
+    "url": "/Movies-App/static/js/main.eeb8aa4b.chunk.js"
+  },
+  {
+    "revision": "ed8df1de7f16c36d8966",
+    "url": "/Movies-App/static/js/runtime~main.8d416b0d.js"
+  },
+  {
+    "revision": "ef94b9673bbf5f1290718772d546bc30",
+    "url": "/Movies-App/static/media/powered-by-square-blue.ef94b967.svg"
+  },
+  {
+    "revision": "281f6d241d2971b514806517d5b9dd84",
+    "url": "/Movies-App/static/media/powered-by-square-green.281f6d24.svg"
+  }
+]);
